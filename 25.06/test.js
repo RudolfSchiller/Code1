7328de097fca80bd66a55d3eebebdffc910c73dc
Rudolf Schiller <rudolf.schiller@gmx.net>
2025-06-30 14:00:25 +0200

    30-06

--- 25.06/test.js
+++ 25.06/test.js
@@ -1,30 +1,38 @@
 "use strict";
-console.log("Ausgabe");
-let y = 1;
+/*console.log("Ausgabe");
+
+let y: number = 1;
+
 {
-    console.log("Wusel-Dusel  \n   \n  ");
+    console.log("Wusel-Dusel  \n   \n  ")
 }
-console.log("consolemaths");
+console.log("consolemaths")
 {
-    let v = 0;
+    let v: number = 0;
     {
-        let v = 1;
+        let v: number = 1;
         v = v;
         console.log(v);
     }
     console.log(v + " \n   \n  ");
+
 }
+
 console.log("IfSchleife");
+
 {
+
     if (y == 1) {
-        console.log("HiHi \n   \n ");
+        console.log("HiHi \n   \n ")
     }
     else {
-        console.log("NoTHiHi \n   \n  ");
+        console.log("NoTHiHi \n   \n  ")
     }
+
 }
 console.log("SwitchSchleife");
 {
+
     switch (y) {
         case 1:
             console.log("HiHi \n   \n ");
@@ -34,6 +42,79 @@ console.log("SwitchSchleife");
             break;
         default:
             console.log("Smug \n   \n ");
+            break;
+    }
+}*/
+// const randomInt = (min: number , max: number) =>
+//  Math.floor(Math.random() * (max - min +1)+ min);
+/*let e = prompt("What number u wanna change");
+
+if (e == "a") {
+
+    let EsubtA: number = parseInt(prompt("how much u wanna subtract") || "1", 10);
+    a = a - EsubtA;
+
+}
+else if (e == "b") {
+        let EsubtB: number = parseInt(prompt("how much u wanna subtract") || "1", 10);
+    b = b - EsubtB;
+}
+else if(e == "c") {
+        let EsubtC: number = parseInt(prompt("how much u wanna subtract") || "1", 10);
+    c = c - EsubtC;
+}
+else if(e == "d") {
+        let EsubtD: number = parseInt(prompt("how much u wanna subtract") || "1", 10);
+    d = d - EsubtD;
+}else{console.log("input was not a number")};
+
+if (a == 0 && b == 0 && c == 0 && d == 0){
+
+    console.log("won");
+};
+*/
+let playerstate = prompt("Enter Name Player 1");
+let playerstate2 = prompt("Enter Name Player 2");
+let activePlayer = playerstate;
+console.log(activePlayer);
+let a = parseInt(prompt("Enter a", "1") || "1", 10);
+let b = parseInt(prompt("Enter b", "1") || "1", 10);
+let c = parseInt(prompt("Enter c", "1") || "1", 10);
+let d = parseInt(prompt("Enter d", "1") || "1", 10);
+console.log("a b c d");
+console.log(a, b, c, d);
+while ((a != 0 || b != 0 || c != 0 || d != 0) == true) {
+    let e = prompt("What number u wanna change");
+    if (e == "a") {
+        let EsubtA = parseInt(prompt("how much u wanna subtract") || "1", 10);
+        a = a - EsubtA;
+    }
+    else if (e == "b") {
+        let EsubtB = parseInt(prompt("how much u wanna subtract") || "1", 10);
+        b = b - EsubtB;
+    }
+    else if (e == "c") {
+        let EsubtC = parseInt(prompt("how much u wanna subtract") || "1", 10);
+        c = c - EsubtC;
+    }
+    else if (e == "d") {
+        let EsubtD = parseInt(prompt("how much u wanna subtract") || "1", 10);
+        d = d - EsubtD;
+    }
+    console.log("a b c d");
+    console.log(a, b, c, d);
+    if (a == 0 && b == 0 && c == 0 && d == 0) {
+        console.log(activePlayer + "won the game");
+        break;
+    }
+    switch (activePlayer) {
+        case playerstate:
+            activePlayer = playerstate2;
+            console.log(playerstate2 + "turn");
+            break;
+        case playerstate2:
+            activePlayer = playerstate;
+            console.log(playerstate + "turn");
             break;
     }
 }
--- 25.06/test.ts
+++ 25.06/test.ts
@@ -1,4 +1,4 @@
-console.log("Ausgabe");
+/*console.log("Ausgabe");
 
 let y: number = 1;
 
@@ -14,20 +14,20 @@ console.log("consolemaths")
         console.log(v);
     }
     console.log(v + " \n   \n  ");
-   
+
 }
 
 console.log("IfSchleife");
 
 {
-    
+
     if (y == 1) {
         console.log("HiHi \n   \n ")
     }
     else {
         console.log("NoTHiHi \n   \n  ")
     }
- 
+
 }
 console.log("SwitchSchleife");
 {
@@ -43,7 +43,100 @@ console.log("SwitchSchleife");
             console.log("Smug \n   \n ");
             break;
     }
+}*/
+
+// const randomInt = (min: number , max: number) =>
+//  Math.floor(Math.random() * (max - min +1)+ min);
+
+/*let e = prompt("What number u wanna change");
+
+if (e == "a") {
+
+    let EsubtA: number = parseInt(prompt("how much u wanna subtract") || "1", 10);
+    a = a - EsubtA;
+
+}
+else if (e == "b") {
+        let EsubtB: number = parseInt(prompt("how much u wanna subtract") || "1", 10);
+    b = b - EsubtB;
+}
+else if(e == "c") {
+        let EsubtC: number = parseInt(prompt("how much u wanna subtract") || "1", 10);
+    c = c - EsubtC;
+}
+else if(e == "d") {
+        let EsubtD: number = parseInt(prompt("how much u wanna subtract") || "1", 10);
+    d = d - EsubtD;
+}else{console.log("input was not a number")};
+
+if (a == 0 && b == 0 && c == 0 && d == 0){
+
+    console.log("won");
+};
+*/
+
+let playerstate = prompt("Enter Name Player 1");
+let playerstate2 = prompt("Enter Name Player 2");
+let activePlayer = playerstate;
+
+    console.log(activePlayer);
+
+let a: number = parseInt(prompt("Enter a", "1") || "1", 10);
+let b: number = parseInt(prompt("Enter b", "1") || "1", 10);
+let c: number = parseInt(prompt("Enter c", "1") || "1", 10);
+let d: number = parseInt(prompt("Enter d", "1") || "1", 10);
+
+
+console.log("a b c d");
+console.log(a, b, c, d);
+
+
+while ((a != 0 || b != 0 || c != 0 || d != 0) == true) {
+    
+    let e = prompt("What number u wanna change");
+
+    if (e == "a") {
+
+        let EsubtA: number = parseInt(prompt("how much u wanna subtract") || "1", 10);
+        a = a - EsubtA;
+
+    }
+    else if (e == "b") {
+        let EsubtB: number = parseInt(prompt("how much u wanna subtract") || "1", 10);
+        b = b - EsubtB;
+    }
+    else if (e == "c") {
+        let EsubtC: number = parseInt(prompt("how much u wanna subtract") || "1", 10);
+        c = c - EsubtC;
+    }
+    else if (e == "d") {
+        let EsubtD: number = parseInt(prompt("how much u wanna subtract") || "1", 10);
+        d = d - EsubtD;
+    }
+
+console.log("a b c d");
+console.log(a, b, c, d);
+
+    if (a == 0 && b == 0 && c == 0 && d == 0) {
+
+       console.log(activePlayer + "won the game");
+break
+    }
+
+    switch (activePlayer) {
+        case playerstate:
+            activePlayer = playerstate2;
+            console.log(playerstate2 +"turn")
+            break;
+
+        case playerstate2:
+            activePlayer = playerstate;
+            console.log(playerstate + "turn");
+            break;
+    }
+
 }
 
 
 
+    
